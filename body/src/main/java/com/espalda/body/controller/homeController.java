677a201fe62f79a010107a3c4eb39108c38c2c7b
espalda <espalda@naver.com>
2021-05-25 17:33:48 +0900

    log4j2.xml 환경설정

--- body/src/main/java/com/espalda/body/controller/homeController.java
+++ body/src/main/java/com/espalda/body/controller/homeController.java
@@ -1,10 +1,16 @@
 package com.espalda.body.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.espalda.body.service.memberService;
 
 /**
  * Handles requests for the application home page.
@@ -12,11 +18,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class homeController {
 	
+	@Autowired
+	memberService memberservice;
 	private static final Logger logger = LoggerFactory.getLogger(homeController.class);
 
 	/** 메인 홈 화면 */
-	@RequestMapping(value="/")
+	@RequestMapping(value="/body")
 	public ModelAndView main(ModelAndView mv){
+		List <String> list = new ArrayList<String> ();
+		list = memberservice.selectTestList();
+		System.out.println("list : " + list);
 	    mv.setViewName("/pages/main/home");
 	    return mv;
 	}
